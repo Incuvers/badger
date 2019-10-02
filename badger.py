59847ea7804caeb731940a65bbce30069d0a4e80
davidsean <david.sean@gmail.com>
2019-10-02 17:29:35 -0400

    add file headers and __init__

--- badger.py
+++ badger.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  Copyright (C) Incuvers, Inc - All Rights Reserved
+#  Unauthorized copying of this file, via any medium is strictly prohibited
+#  Proprietary and confidential
+#  Written by David Sean
+#
 
 import svgwrite as sw
 from svgwrite import px
